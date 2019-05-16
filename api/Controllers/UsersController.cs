98d51dbd676e03962d600932948cfb2c913a958c
Hamish Wadsworth <hamishwaddy@gmail.com>
2019-05-16 16:24:02 +1200

    finished users controller

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly MoviesContext _context;
+
+        public UsersController(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movies>>> GetUsers()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(long id)
+        {   
+            Users item = await _context.Users.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUsers(Users item)
+        {
+            _context.Users.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+              nameof(GetUsers), 
+              item);
+        }
+
+        [HttpPut ("{id}")]
+         public async Task<IActionResult> PutTodoItem (int id, Users item) {
+         if (id != item.Id) {
+         return BadRequest ();
+         }
+         _context.Entry (item).State = EntityState.Modified;
+         await _context.SaveChangesAsync ();
+         return Content ("User has been updated");
+         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsersItem(short id)
+        {
+            Users model = await _context.Users.FindAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return Content("User has been removed");
+        }
+    }
+}
